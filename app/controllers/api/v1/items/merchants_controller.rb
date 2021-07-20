4139b1724c6d0b95effa6f1c1bf301f9f7aaea66
Suzie K <suziekim.dev@gmail.com>
2021-07-20 15:48:37 -0400

    Update specs

---
+++ app/controllers/api/v1/items/merchants_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::Items::MerchantsController < ApplicationController
+  
+  def index
+    item = Item.find(params[:item_id])
+    render json: MerchantSerializer.new(item.merchant)
+ 
+    rescue ActiveRecord::RecordNotFound
+      render json: {
+        message: 'Not Found',
+        errors: ["Could not find merchant with id##{params[item.merchant.id]}"]
+      }, status: :not_found
+  end
+end
--- spec/requests/api/v1/items/merchants_request_spec.rb
+++ spec/requests/api/v1/items/merchants_request_spec.rb
@@ -2,18 +2,33 @@ require 'rails_helper'
 
 RSpec.describe "Item Merchant API" do
   context 'item merchant index' do
-    it 'happy path: ' do
+    it 'happy path: can find item/s merchant' do
       merchant = create(:mock_merchant)
-      create_list(:mock_item, 25, merchant: merchant)
+      item = create(:mock_item, merchant: merchant)
       
-      get '/api/v1/items'
+      get "/api/v1/items/#{item.id}/merchants" 
 
       expect(response).to be_successful
 
       body = JSON.parse(response.body, symbolize_names: true)
 
-      expect(items[:data].size).to eq(20)
+      expect(body[:data][:id]).to eq(merchant.id.to_s)
+    end
+
+    # this test is actually instantiating a merchant
+    xit 'sad path: cannot find item/s merchant' do
+      merchant = create(:mock_merchant)
+      item = create(:mock_item, merchant: merchant)
+
+      get "/api/v1/items/#{item.id}/merchants" 
+
+      expect(response).to be_successful
+
+      body = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(body[:message]).to eq 'Not Found'
+      expect(body[:errors]).to include 'Could not find merchant with id#0'
+      expect(response.status).to eq(404)
     end
   end
-
 end
