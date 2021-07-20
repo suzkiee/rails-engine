7067f3699a01bd5560c1d8e8427179ed800b0235
Suzie K <suziekim.dev@gmail.com>
2021-07-20 10:50:14 -0400

    Red test merchant show

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -18,4 +18,29 @@ RSpec.describe "Items API" do
       expect(merchants[:data].size).to eq(20)
     end
   end
+
+  context 'merchant show' do
+    it 'happy path: can find a single merchant' do
+      create_list(:mock_merchant, 25)
+      merchant = Merchant.first
+      get "/api/v1/merchants/#{merchant.id}"
+      
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(body[:data][:id]).to eq("#{merchant.id}")
+      expect(body[:data][:type]).to eq("merchant")
+      expect(body[:data][:attributes][:name]).to eq("Sawyer B. Hind")
+    end
+
+    it 'sad path: cannot find the merchant' do
+      id = 0
+      get "/api/v1/merchants/#{id}"
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(body[:message]).to eq 'Not Found'
+      expect(body[:errors]).to include 'Could not find merchant with id#0'
+      expect(response.status).to eq(404)
+    end
+  end
 end
