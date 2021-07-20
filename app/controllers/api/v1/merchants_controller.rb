26fb01925f4ee93e317c71edc6dcabc8a56d978c
Suzie K <suziekim.dev@gmail.com>
2021-07-20 15:16:43 -0400

    Update merchant index method

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,9 +1,19 @@
 class Api::V1::MerchantsController < ApplicationController
-  MERCHANTS_PER_PAGE = 20
-
+  
   def index
-    page = params.fetch(:page, 1).to_i
-    merchants = Merchant.offset((page - 1) * MERCHANTS_PER_PAGE).limit(MERCHANTS_PER_PAGE)
+    if params[:page].nil? 
+      page = params.fetch(:page, 1).to_i
+    else 
+      page = params[:page].to_i
+    end
+
+    if params[:per_page].nil?
+      limit = 20 
+    else 
+      limit = params[:per_page].to_i
+    end
+
+    merchants = Merchant.offset((page - 1) * limit).limit(limit)
     render json: MerchantSerializer.new(merchants)
   end
 
