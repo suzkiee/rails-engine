147a6527bdfd38a1beb0ffa20fb4fe18620908c4
Suzie K <suziekim.dev@gmail.com>
2021-07-20 10:50:19 -0400

    Green test merchant show

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,7 +3,18 @@ class Api::V1::MerchantsController < ApplicationController
 
   def index
     page = params.fetch(:page, 1).to_i
-    merchants = Merchant.offset(page * MERCHANTS_PER_PAGE).limit(MERCHANTS_PER_PAGE)
+    merchants = Merchant.offset((page - 1) * MERCHANTS_PER_PAGE).limit(MERCHANTS_PER_PAGE)
     render json: MerchantSerializer.new(merchants)
+  end
+
+  def show
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant) 
+
+  rescue ActiveRecord::RecordNotFound
+    render json: {
+      message: 'Not Found',
+      errors: ["Could not find merchant with id##{params[:id]}"]
+    }, status: :not_found
   end
 end
