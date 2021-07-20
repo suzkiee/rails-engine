5f6e128ef6c681325fafe0ca4273d29fae1fd6e6
Suzie K <suziekim.dev@gmail.com>
2021-07-20 11:41:01 -0400

    Red test returns all items belonging to merchant

--- spec/requests/api/v1/merchants/items_request_spec.rb
+++ spec/requests/api/v1/merchants/items_request_spec.rb
@@ -2,16 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant Items API' do
   context 'merchant items index' do
-    merchant = create(:merchant)
-    create_list(:item, 10, merchant: merchant)
+    it 'returns all items belonging to merchant' do
+      merchant = create(:mock_merchant)
+      create_list(:mock_item, 10, merchant: merchant)
+      items = merchant.items
 
-    get "/api/v1/merchants/#{merchant.id}/items"
+      get "/api/v1/merchants/#{merchant.id}/items"
+    
+      body = JSON.parse(response.body, symbolize_names: true)
 
-    body = JSON.parse(response.body, symbolize_names: true)
-
-    expect(response).to be_successful
-    expect(body[:data].size).to eq(10)
-    expect(body[:data].first[:attributes][:name]).to eq(Item.first.name)
-    expect(body[:data].last[:attributes][:name]).to eq(Item.last.name)
+      expect(response).to be_successful
+      expect(body[:data].size).to eq(10)
+      expect(body[:data].first[:attributes][:name]).to eq(items.first.name)
+      expect(body[:data].last[:attributes][:name]).to eq(items.last.name)
+    end
   end
 end
