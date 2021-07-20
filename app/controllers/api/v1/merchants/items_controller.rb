a776890d50bf4045bfc7aafe9dc1ca980b5d9574
Suzie K <suziekim.dev@gmail.com>
2021-07-20 11:58:05 -0400

    Green test sad path: cannot find merchant

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -3,5 +3,11 @@ class Api::V1::Merchants::ItemsController < ApplicationController
   def index
     items = Merchant.find(params[:merchant_id]).items
     render json: ItemSerializer.new(items)
+    
+    rescue ActiveRecord::RecordNotFound
+    render json: {
+      message: 'Not Found',
+      errors: ["Could not find merchant with id##{params[:merchant_id]}"]
+    }, status: :not_found
   end
 end
