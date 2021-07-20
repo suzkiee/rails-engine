649115de97851c813e5a440eee191d00eb84cc48
Suzie K <suziekim.dev@gmail.com>
2021-07-20 10:52:03 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :items, except: [:new, :edit]
-      resources :merchants, only: [:index, :show]
+      resources :items, except: [:new, :edit] do
+        resources :merchants, only: [:index]
+      end
+      
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index] 
+      end
     end
   end
 end
