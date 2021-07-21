92a94dcd283fefdc4add7ac6ef54d332bc2d59c7
Suzie K <suziekim.dev@gmail.com>
2021-07-21 16:27:29 -0400

    Green test potential revenue

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,4 +7,11 @@ class Invoice < ApplicationRecord
   has_many :items, through: :invoice_items
   has_many :transactions, dependent: :destroy
   has_many :merchants, through: :items
+
+  def self.potential_revenue
+    joins(:invoice_items)
+    .select("invoices.id as id, sum(invoice_items.quantity * invoice_items.unit_price) as revenue")
+    .where.not(status: 'shipped')
+    .group(:id)
+  end
 end
