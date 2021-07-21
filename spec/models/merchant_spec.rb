c61513289e49df7f733bf8858f91451c5abc3eb6
Suzie K <suziekim.dev@gmail.com>
2021-07-21 16:28:42 -0400

    Red test most_revenue

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -11,6 +11,47 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:invoice_items) }
     it { should have_many(:transactions) }
   end
+  
+  describe 'instance methods' do
+    describe "::most_revenue" do
+      it 'returns merchant with most revenue' do
+        merchant_1 = create(:mock_merchant)
+        merchant_2 = create(:mock_merchant)
+        merchant_3 = create(:mock_merchant)
+  
+        customer_1 = create(:mock_customer)
+        customer_2 = create(:mock_customer)
+        customer_3 = create(:mock_customer)
+  
+        item_1 = create(:mock_item, merchant: merchant_1)
+        item_2 = create(:mock_item, merchant: merchant_2)
+        item_3 = create(:mock_item, merchant: merchant_3)
+  
+        invoice_1 = create(:mock_invoice, merchant: merchant_1, customer: customer_1, status: 'shipped')
+        invoice_2 = create(:mock_invoice, merchant: merchant_2, customer: customer_2, status: 'shipped')
+        invoice_3 = create(:mock_invoice, merchant: merchant_3, customer: customer_3, status: 'shipped')
+  
+        invoice_item_1 = create(:mock_invoice_item, item: item_1, invoice: invoice_1, quantity: 12, unit_price: 1)
+        invoice_item_2 = create(:mock_invoice_item, item: item_2, invoice: invoice_2, quantity: 20, unit_price: 2)
+        invoice_item_3 = create(:mock_invoice_item, item: item_3, invoice: invoice_3, quantity: 10, unit_price: 3)
+  
+        transaction_1 = create(:mock_transaction, invoice: invoice_1, result: 'success')
+        transaction_2 = create(:mock_transaction, invoice: invoice_2, result: 'success')
+        transaction_3 = create(:mock_transaction, invoice: invoice_3, result: 'success')
+        
+        first = Merchant.most_revenue.first
+        last = Merchant.most_revenue.last
+
+        expect(first.id).to eq(merchant_2.id)
+        expect(first.name).to eq(merchant_2.name)
+        expect(first.revenue).to eq(merchant_2.revenue)
+        
+        expect(last.id).to eq(merchant_1.id)
+        expect(last.name).to eq(merchant_1.name)
+        expect(last.revenue).to eq(merchant_1.revenue)
+      end
+    end
+  end
 
   describe 'instance methods' do
     describe '#revenue' do
