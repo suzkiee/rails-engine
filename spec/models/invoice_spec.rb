5c854834f49094ca776eb6caa76748851dd56850
Suzie K <suziekim.dev@gmail.com>
2021-07-21 16:27:21 -0400

    Red test find potential revenue

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -12,4 +12,40 @@ RSpec.describe Invoice do
     it { should have_many :transactions }
     it { should have_many(:items).through(:invoice_items) }
   end
+
+  describe "class methods" do
+    describe '::potential_revenue' do
+      it 'returns potential revenue of invoices' do
+        merchant = create(:mock_merchant)
+
+        customer_1 = create(:mock_customer)
+        customer_2 = create(:mock_customer)
+        customer_3 = create(:mock_customer)
+
+        item_1 = create(:mock_item, merchant: merchant)
+        item_2 = create(:mock_item, merchant: merchant)
+        item_3 = create(:mock_item, merchant: merchant)
+
+        invoice_1 = create(:mock_invoice, merchant_id: merchant.id, customer: customer_1, status: 'packaged')
+        invoice_2 = create(:mock_invoice, merchant_id: merchant.id, customer: customer_1, status: 'packaged')
+        invoice_3 = create(:mock_invoice, merchant_id: merchant.id, customer: customer_2, status: 'shipped')
+        invoice_4 = create(:mock_invoice, merchant_id: merchant.id, customer: customer_2, status: 'packaged')
+        invoice_5 = create(:mock_invoice, merchant_id: merchant.id, customer: customer_3, status: 'shipped')
+        invoice_6 = create(:mock_invoice, merchant_id: merchant.id, customer: customer_3, status: 'packaged')
+
+        invoice_item_1 = create(:mock_invoice_item, item: item_1, invoice: invoice_1, quantity: 18, unit_price: 1000.00)
+        invoice_item_2 = create(:mock_invoice_item, item: item_2, invoice: invoice_2, quantity: 12, unit_price: 20000.99)
+        invoice_item_3 = create(:mock_invoice_item, item: item_3, invoice: invoice_3, quantity: 5, unit_price: 24403.91)
+        invoice_item_4 = create(:mock_invoice_item, item: item_1, invoice: invoice_4, quantity: 10, unit_price: 100)
+        invoice_item_5 = create(:mock_invoice_item, item: item_2, invoice: invoice_5, quantity: 2, unit_price: 4000)
+        invoice_item_5 = create(:mock_invoice_item, item: item_3, invoice: invoice_6, quantity: 5, unit_price: 80)
+        
+        transaction_1 = create(:mock_transaction, invoice: invoice_1, result: 'success')
+        transaction_2 = create(:mock_transaction, invoice: invoice_2, result: 'success')
+        transaction_3 = create(:mock_transaction, invoice: invoice_3, result: 'success')
+        
+        expect(Invoice.potential_revenue).to eq(259411.88)
+      end
+    end
+  end
 end
