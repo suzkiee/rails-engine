4b85cbf4f7244b02b801f99a0fb46dd73254a943
Suzie K <suziekim.dev@gmail.com>
2021-07-21 11:24:36 -0400

    Green test revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,9 +1,25 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
   
-  has_many :items, dependent: :destroy
+  has_many :items, dependent: :destroy  
+  has_many :invoices, dependent: :destroy
   has_many :invoice_items, through: :items
-  has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
+
+  def revenue
+    invoices
+    .joins(:transactions)
+    .joins(:invoice_items)
+    .where("transactions.result = 'success' and invoices.status = 'shipped'")
+    .sum('invoice_items.quantity * invoice_items.unit_price')
+    
+    # invoices
+    # .joins(:transactions)
+    # .joins(:invoice_items)
+    # .where("invoices.status = 'shipped' and transactions.result = 'success'")
+    # .select("sum(invoice_items.quantity * invoice_items.unit_price) as revenue,
+    #                     items.merchant_id as merchant_id")
+    # .group('items.merchant_id')
+  end
 end
