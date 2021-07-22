4d0b5652588b51ea58edf5ca02ec241f9d17d3e5
Suzie K <suziekim.dev@gmail.com>
2021-07-22 17:03:05 -0400

    Edit test for rank_most_revenue

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -24,5 +24,33 @@ RSpec.describe Item, type: :model do
         expect(Item.search("ring")).to eq([item, item_2])
       end
     end
+
+    describe '::rank_most_revenue' do
+      it 'returns items with the most revenue' do
+        merchant_1 = create(:mock_merchant)
+
+        customer_1 = create(:mock_customer)
+        customer_2 = create(:mock_customer)
+        customer_3 = create(:mock_customer)
+
+        item_1 = create(:mock_item, merchant: merchant_1)
+        item_2 = create(:mock_item, merchant: merchant_2)
+        item_3 = create(:mock_item, merchant: merchant_3)
+
+        invoice_1 = create(:mock_invoice, merchant: merchant_1, customer: customer_1)
+        invoice_2 = create(:mock_invoice, merchant: merchant_2, customer: customer_2)
+        invoice_3 = create(:mock_invoice, merchant: merchant_3, customer: customer_3)
+
+        invoice_item_1 = create(:mock_invoice_item, item: item_1, invoice: invoice_1, unit_price: 1)
+        invoice_item_2 = create(:mock_invoice_item, item: item_2, invoice: invoice_2, unit_price: 2)
+        invoice_item_3 = create(:mock_invoice_item, item: item_3, invoice: invoice_3, unit_price: 3)
+
+        transaction_1 = create(:mock_transaction, invoice: invoice_1)
+        transaction_2 = create(:mock_transaction, invoice: invoice_2)
+        transaction_3 = create(:mock_transaction, invoice: invoice_3)
+
+        expect(Item.rank_most_revenue).to eq(0)
+      end
+    end
   end
 end
